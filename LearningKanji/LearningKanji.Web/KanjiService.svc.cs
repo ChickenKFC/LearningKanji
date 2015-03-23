36fee5ca1048837a5442cf75d68bfc8412e61af3
Trâu Đại Ca <nhatanh.thpt@gmail.com>
2015-03-23 23:09:13 +0700

    Create project

---
+++ LearningKanji/LearningKanji.Web/KanjiService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LearningKanji.Web
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "KanjiService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select KanjiService.svc or KanjiService.svc.cs at the Solution Explorer and start debugging.
+    public class KanjiService : IKanjiService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
